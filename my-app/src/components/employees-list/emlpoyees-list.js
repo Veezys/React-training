733a6144a749d7b75c0ad04f5688ff46e462634f
Babu Readymade <breadyma@pr-m5.kzn.21-school.ru>
2022-05-13 19:33:50 +0300

    feat: made verst

---
+++ my-app/src/components/employees-list/emlpoyees-list.js
@@ -0,0 +1,16 @@
+import EmployeesListItem from '../employees-list-item/employees-list-item';
+
+import './employees-list.css';
+
+
+function EmployeesList () {
+	return (
+		<div className="app-list">
+			<EmployeesListItem/>
+			<EmployeesListItem/>
+			<EmployeesListItem/>
+		</div>
+	);
+}
+
+export default EmployeesList;
