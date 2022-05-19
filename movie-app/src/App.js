7a0d375227f977bd58f6fb71cc21f8540ed34135
Babu Readymade <breadyma@un-j6.kzn.21-school.ru>
2022-05-19 18:57:19 +0300

    feat movie-app: axile API

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,15 +1,46 @@
 import React from "react";
+import axios from 'axios';
+import Movie from "./Movie";
 
  class App extends React.Component {
 
-  
+  state = {
+    isLoading: true,
+    movie: [],
+  };
+
+  getMoives = async () => {
+    const {data: {data: {movies}}} =  await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false}) 
+  }
+
+  componentDidMount () {
+    this.getMoives();
+  }
+
+
   render () {
+    const {isLoading, movies} = this.state;
     return (
-      <div className="App">
-        
-      </div>
+      <section className="container" >
+        {isLoading ? <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div> : movies.map(movie => {
+         return <div className="movies">
+            <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}
+            genres={movie.genres}/>
+         </div>
+        })}
+      </section>
     );
   }
 }
 
 export default App;
+ 
---
+++ movie-app/src/Movie.js
@@ -0,0 +1,21 @@
+
+
+function Movie({id, year, title, summary, poster, genres}) {
+	return (
+		<div className="movie">
+			<img src={poster} alt={title} />
+			<div className="movie__column">
+				<h3 className="movie__title">{title}</h3>
+				<h5 className="movie__year">{year}</h5>
+				<p className="movie__summary">{summary}</p>
+				<ul className="movie__genres">{genres.map((genres, id) => {
+					return <li key={id} className="movie__item">{genres}</li>
+				})}
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+export default Movie;
+
